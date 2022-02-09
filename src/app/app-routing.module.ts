7dee89a35dd6f6ecd941aeceb731e76b9aa0da1f
astechutube <astech.utube@gmail.com>
2022-02-09 10:15:37 +0530

    angular-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  { path: '', component: UserComponent },
-  { path: 'user', component: UserComponent },
-  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule
+    AppRoutingModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
